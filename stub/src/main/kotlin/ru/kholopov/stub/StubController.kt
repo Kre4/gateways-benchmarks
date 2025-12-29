c53dc8b995fd02e00b1bc9253c59fc50e626d73c
Denis.Kholopov <denis.holopov@krit.pro>
2025-12-29 22:05:02 +0300

    feat: add scenarios to gateways config

--- stub/src/main/kotlin/ru/kholopov/stub/StubController.kt
+++ stub/src/main/kotlin/ru/kholopov/stub/StubController.kt
@@ -11,6 +11,11 @@ import kotlin.time.toJavaDuration
 @RestController
 class StubController {
 
+    data class JsonResponse(
+        val id: Int = 42,
+        val payload: String,
+    )
+
     @GetMapping("/success")
     fun alwaysSuccess(): ResponseEntity<Unit> {
         return ResponseEntity.status(HttpStatus.OK).build();
@@ -25,5 +30,12 @@ class StubController {
     fun withDelay(@RequestParam(required = false) delayMillis: Int?) {
         val delay = delayMillis ?: 1_000
         Thread.sleep(delay.milliseconds.toJavaDuration())
+    }
+
+    @GetMapping("/api/v1/json")
+    fun json(@RequestParam(required = false) payloadSize: Int?): ResponseEntity<JsonResponse> {
+        val size = (payloadSize ?: 1024).coerceIn(0, 100_000)
+        val payload = "a".repeat(size)
+        return ResponseEntity.ok(JsonResponse(payload = payload))
     }
 }
